bb9990065dce99d1bd1a45dfed87d747291e9218
Tina <tina.winnn@gmail.com>
2023-01-31 22:11:22 -0500

    round function running

--- script.js
+++ script.js
@@ -1,25 +1,37 @@
-// declaring computer choices //
-const choices = ["rock", "paper", "scissors"];
-
-// function randomly chooses item in array //
-function getComputerChoice (choices) {
-    return choices[Math.floor(Math.random()*choices.length)];
+// 
+function getComputerChoice () {
+    const choices = ["rock", "paper", "scissors"];
+    let choice = choices[Math.floor(Math.random()*choices.length)];
+    return choice;
 }
 
-//Write a function that plays a single round //
+//Write a function that plays a single round //\
 function playRound (playerSelection, computerSelection) {
-    playerSelection = prompt("Choose your selection", " ");
-    return playerSelection.toLowerCase("");
-
-//compare player selection and computer selection //
-
-
+    if (playerSelection === computerSelection) {
+        console.log("Eek! It's a Tie!");
+    } else if ((playerSelection === "rock") && (computerSelection === "scissors")) {
+        console.log("You win! Rock beats Scissors!");
+    } else if ((playerSelection === "paper") && (computerSelection === "rock")) {
+        console.log("You win! Paper beats Rock!");
+    } else if ((playerSelection === "scissors") && (computerSelection === "paper")) {
+        console.log("You win! Scissors beats Paper! ");
+    } else if ((computerSelection === "paper") && (playerSelection === "rock")) {
+        console.log("You Lose! Paper beats Rock!");
+    } else if ((computerSelection === "scissors") && (playerSelection === "paper")) {
+        console.log("You Lose! Scissors beats Paper!");
+    } else if ((computerSelection === "rock") && (playerSelection === "scissors")) {
+        console.log("You Lose! Rock beats Scissors!");
+    }
 }
-console.log(playRound(prompt))
 
+playerSelection = window.prompt("Choose your selection").toLowerCase("");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
 
 
 // game function to play 5 rounds and declare winner 
+// playerSelection = window.prompt("Choose your selection").toLowerCase("");
+
 
 
 
